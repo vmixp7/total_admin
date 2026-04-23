37c2ce5cb540ba92f95bd972a180868499d4a487
p7 <vmixp7@gmail.com>
2026-04-23 18:15:22 +0800

    edit base url

--- web/js/script.js
+++ web/js/script.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://admin.fuchslube.com.tw";
+const BASE_URL = "https://admin.totallube.com.tw";
 // const BASE_URL = "http://127.0.0.1:5000";
 const FORMOSA_URL = "https://www.formosalube.com.tw";
